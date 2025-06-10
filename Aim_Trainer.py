d29ba5a6e432badffb209500220985a11413776c
RashmiAyodhya <jennie20.23rak@gmail.com>
2025-06-10 12:36:59 +0530

    Fix caption, event name, and font size

--- Aim_Trainer.py
+++ Aim_Trainer.py
@@ -6,15 +6,14 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.set_caption("Aim Trainer")  
+pygame.display.set_caption("Aim Trainer")
 
-TARGET_INCREMENT = "400"  
-TARGET_EVENT = pygame.USER_EVENT  
-
+TARGET_INCREMENT = 400
+TARGET_EVENT = pygame.USEREVENT
 TARGET_PADDING = 30
 
 BG_COLOR = (0, 25, 40)
 LIVES = 3
 TOP_BAR_HEIGHT = 50
 
-LABEL_FONT = pygame.font.SysFont("comicsans", 22) 
+LABEL_FONT = pygame.font.SysFont("comicsans", 24)
